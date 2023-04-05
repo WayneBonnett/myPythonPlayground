58aa7aa7550ee1b00474251bb1455f90ae5cfb1c
BCPSWayne <125310411+BCPSWayne@users.noreply.github.com>
2023-04-05 10:04:57 -0400

    Initial commit.

---
+++ blackjack/blackjack.py
@@ -0,0 +1,47 @@
+"""Blackjack
+The classic card game also known as 21. (This version doesn't have splitting or insurance.)
+More info at: https://en.wikipedia.org/wiki/Blackjack
+"""
+
+import random
+import sys
+
+#Set up suits
+HEARTS   = chr(9829) # ♥
+DIAMONDS = chr(9830) # ♦
+SPADES   = chr(9824) # ♠
+CLUBS    = chr(9827) # ♣
+BACKSIDE = 'backside' # the back of the cards
+
+def main():
+    print('''Blackjack
+          
+          Rules of this game:
+          Try to reach a score as close to 21 without going over.
+          
+          Card points value
+          Kings  10 points
+          Queens 10 points
+          Jacks  10 points
+          
+          Aces  1 or 11 points
+          Cards 2 through 10 are worth face value
+          
+          (H)it to have the dealer deal another card.
+          (S)tand to stop taking cards from the dealer.
+          
+          On the first play you can (D)ouble down to double your bet
+          but you must (H)it one more time before (S)tanding.
+          
+          The dealer stops betting after hitting 17 or above
+          If there is a tie, the bet is returned to the player.
+          
+          Good luck!
+          ''')
+    money = 10_000
+    
+    print(money)
+
+
+if __name__ == '__main__':
+    main()
